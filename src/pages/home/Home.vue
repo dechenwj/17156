7f28fdee7da5649d92dcacd9a9c1f674a16ad4a0
liudong <2206170151@qq.com>
2017-11-03 22:14:32 +0800

    locationActivity

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,6 +2,7 @@
 	<div>
   		<home-header></home-header>
 		<swiper-content></swiper-content>
+		<location-Activity></location-Activity>
 		<hot-Recommend></hot-Recommend>
 	</div>
 </template>
@@ -11,11 +12,13 @@
 import HeaderComponent from "./Header";
 import SwiperComponent from "./Swiper";
 import HotRecommendComponent from "./HotRecommend";
+import locaActivityComponent from "./locaActivity";
 
 export default {
 	components: {
 		"home-header": HeaderComponent,
 		"swiper-content": SwiperComponent,
+		"location-Activity":locaActivityComponent,
 		"hot-Recommend":HotRecommendComponent
 	}
 }
