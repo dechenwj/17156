134b8aa9eb04fc5a8d0c54a8f724a8d44d47f6e2
wenjingchen <wenjingchen@yeah.net>
2017-11-04 14:50:29 +0800

    suchlist

---
+++ src/pages/search/Search.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+  		<search-header></search-header>
+		<search-content></search-content>
+	</div>
+</template>
+
+<script>
+
+import HeaderComponent from "./Header";
+import ContentComponent from "./Content";
+
+export default {
+	components: {
+		"search-header": HeaderComponent,
+		"search-content": ContentComponent
+	}
+}
+</script>
+
+<style>
+ 
+</style>
